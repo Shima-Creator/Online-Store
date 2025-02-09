1a7f7d0b7af989a3905a85b8d68ab1c54a7191d3
Shima-Creator <shima.228@outlook.com>
2025-02-10 02:45:56 +0300

    Added users urls

---
+++ users/urls.py
@@ -0,0 +1,20 @@
+from itertools import product
+
+from debug_toolbar.urls import app_name
+from django.contrib.auth.views import LogoutView
+from pygments.lexer import include
+
+from users import views
+
+from django.urls import path
+
+from users.views import LogoutUserView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.LoginUserView.as_view(), name='login'),
+    path('logout/', views.LogoutUserView.as_view(), name='logout'),
+    path('register/', views.RegisterUserView.as_view(), name='register'),
+]
+
