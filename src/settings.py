2048471cfeb90125f77aeec531339440f81b6517
Shima-Creator <shima.228@outlook.com>
2025-03-09 20:12:14 +0300

    Added some functional for user

--- src/settings.py
+++ src/settings.py
@@ -163,6 +163,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
-from users import views
+from django.urls import include
 
+from users import views
 from django.urls import path
 
 app_name = 'users'
@@ -9,4 +10,3 @@ urlpatterns = [
     path('logout/', views.LogoutUserView.as_view(), name='logout'),
     path('register/', views.RegisterUserView.as_view(), name='register'),
 ]
-
