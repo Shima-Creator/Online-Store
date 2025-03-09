c65a89581051a93f6a6098bead1dfd3f1caeddb5
Shima-Creator <shima.228@outlook.com>
2025-03-09 19:56:43 +0300

    Added forms for user's categories

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,25 @@
 from django import forms
-from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth.models import User
+
+from users.models import Buyer, Seller, User
+
+
+class UserCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('email',)
+
+
+class SellerRegisterForm(forms.ModelForm):
+    class Meta:
+        model = Seller
+        fields = ['upd', 'legal_name',]
+
+
+class BuyerRegisterForm(forms.ModelForm):
+    class Meta:
+        model = Buyer
+        fields = ['address',]
 
 
 class LoginUserForm(AuthenticationForm):
@@ -9,29 +27,3 @@ class LoginUserForm(AuthenticationForm):
                                widget=forms.TextInput(attrs={'class':'form-input'}))
     password = forms.CharField(label='Пароль',
                                widget=forms.PasswordInput(attrs={'class':'form-input'}))
-
-
-class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
-
-    class Meta:
-        model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
-        labels = {
-            'email': 'E-mail',
-            'first_name': 'Имя',
-            'last_name': 'Фамилия',
-        }
-        widgets = {
-            'email': forms.TextInput(attrs={'class': 'form-input'}),
-            'first_name': forms.TextInput(attrs={'class': 'form-input'}),
-            'last_name': forms.TextInput(attrs={'class': 'form-input'}),
-        }
-
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Такой E-mail уже существует!")
-        return email
